1d2ac465a3a32e5b217760297597e22d6c1eb8de
Vasyl Kazimirov <kazimirov.v@gmail.com>
2018-10-27 01:46:32 +0300

    Setup some headers

--- src/services/index.js
+++ src/services/index.js
@@ -3,5 +3,11 @@ import { BE_BASIC_URL } from '../configs';
 
 export default axios.create({
   baseURL: BE_BASIC_URL,
-  responseType: 'json'
+  responseType: 'json',
+  headers: {
+    common: {
+      'Access-Control-Allow-Origin': BE_BASIC_URL,
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  }
 });
