57139cefd8f1ce7d5b19ef936db408a260f37c55
Vasyl Kazimirov <kazimirov.v@gmail.com>
2018-10-27 15:11:05 +0300

    Add toolbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import MemesList from './components/MemesList/MemesList';
 import SearchField from './components/SearchField/SearchField';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Meme Searcher
+          </Typography>
+          </Toolbar>
+        </AppBar>
         <SearchField />
         <MemesList />
       </div>
